d6d137a5acb2333bcf9129a187908bdfa64551f1
Gene Christian Somoza <somozagenechristian@gmail.com>
2021-06-07 17:34:45 +0800

    Mock external api calls

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,8 +8,14 @@ use Illuminate\Http\Response;
 class UserController extends Controller
 {
     public function login(Request $request) {
-        $apiResponse = '{ "username": "foo", "displayName": "Foo Bar Foo", "roles": [ "basic-user" ] }';
-        // $apiResponse = '{ "message": "Invalid username or password." }';
+        $username = $request->user["username"];
+        $password = $request->user["password"];
+        if($username == "foo" && $password == "bar") {
+            $apiResponse = '{ "username": "foo", "displayName": "Foo Bar Foo", "roles": [ "basic-user" ] }';
+        }
+        else {
+            $apiResponse = '{ "message": "Invalid username or password." }';
+        }
 
         return response($apiResponse, 200)->header('Content-Type', 'application/json');
     }
